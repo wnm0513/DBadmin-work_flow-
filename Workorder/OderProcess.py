b2e5e693c248eec0cd4111fb32861ab57a53d3f2
wnm0513 <musheng.zoey@gmail.com>
2022-03-10 16:02:34 +0800

    2022.3.10

--- Workorder/OderProcess.py
+++ Workorder/OderProcess.py
@@ -1,10 +1,16 @@
+import datetime
+
 from flask import (
     Flask, Blueprint, flash, g, redirect, render_template, request, session, url_for
 )
 
 from app import login_required
+from config import Config
 from useddb.models import WorkFlow, Workorder, db, Departments
 from . import OrderProcesses
+from .MineWorkorder import goinceptionCheck
+
+path = Config.INCEPTION_PATH
 
 
 @OrderProcesses.route('/OrderProcess')
@@ -25,6 +31,7 @@ def OrderProcess():
         deptname = db.session.query(Departments.deptname).filter(Departments.id == workorder.deptid)
         workflow = WorkFlow.query.filter(WorkFlow.woid == workorder.id).first()
         workorderinfo = {
+            'id': workorder.id,
             'uname': workflow.uname,
             'deptname': deptname,
             'stime': workorder.stime,
@@ -35,3 +42,54 @@ def OrderProcess():
         workordersinfo.append(workorderinfo)
 
     return render_template('workorder/OrderProcess/OrderProcess.html', workordersinfo=workordersinfo)
+
+
+@OrderProcesses.route('/OrderDetail/<id>')
+@login_required
+def OrderDetail(id):
+    # 引用全局变量
+    global path
+    # 定义列表
+    sqlsinfo = []
+    
+    # 查询对应的工单
+    workorder = db.session.query(Workorder).filter_by(id=id).first()
+    workflow = db.session.query(WorkFlow).filter_by(woid=id).first()
+    date = datetime.datetime.strptime(str(workorder.stime), '%Y-%m-%d %H:%M:%S').date()
+    orderdate = str(date).replace('-', '')
+
+    # 读取文件
+    filecontent = open('{path}/{day}/{filename}'.format(path=path, day=orderdate, filename=workorder.filename),
+                       'r')
+    allsqls = filecontent.readlines()
+    # 将最后一行的dbname取出来
+    dbnamelist = allsqls.pop().split()
+    filecontent.close()
+    
+    for allsql in allsqls:
+
+        sqlresults = goinceptionCheck(allsql)
+
+        for sqlresult in sqlresults:
+            # 整合check结果
+            sqlinfo = {
+                'stage': sqlresult[1],
+                'error_level': sqlresult[2],
+                'stage_status': sqlresult[3],
+                'error_message': sqlresult[4],
+                'sql': sqlresult[5],
+                'affected_rows': sqlresult[6],
+                'execute_time': sqlresult[9],
+                'backup_time': sqlresult[11]
+            }
+            sqlsinfo.append(sqlinfo)
+
+    # 当别的视图请求时
+    if request.method == 'GET':
+        return render_template('workorder/OrderProcess/OrderDetail.html', sqlsinfo=sqlsinfo, workorder=workorder, workflow=workflow)
+
+
+
+
+
+
--- Workorder/__init__.py
+++ Workorder/__init__.py
@@ -1,3 +1,8 @@
+import datetime
+import os
+from urllib import request
+import json
+import time
 from flask import Blueprint
 
 MineWorkorders = Blueprint('MineWorkorder', __name__)
@@ -6,3 +11,68 @@ OrderProcesses = Blueprint('OrderProcess', __name__)
 
 OrderHistories = Blueprint('OrderHistory', __name__)
 
+
+def get_token():
+    """#获取access_token"""
+    tokenfile = './do_not_change_this_dingfile'
+    expires_in = 3600
+    CROPID = 'dingrhiiq5w6sbza5udq'
+    SECRET = '9M9gECekk-aHMve64lRhxiP7iJlv3oPk0Epm7IUb7ua8cSIsg_rEALHA9sw6Ql-8'
+    url = 'https://oapi.dingtalk.com/gettoken?corpid=%s&corpsecret=%s' % (CROPID, SECRET)
+    if os.path.exists(tokenfile):
+        token_createtime = int(os.path.getctime(tokenfile))
+        nowtime = int(time.time())
+        # 如果key没有过期
+        if nowtime - token_createtime < expires_in:
+            file_token = open(tokenfile, 'r').readlines()
+            token = file_token[0].replace('\n', '')
+            return token
+        else:
+            req = request.Request(url)
+            result = request.urlopen(req)
+            access_token = json.loads(result.read())
+            # 判断是返回了合法的值
+            if access_token.has_key('errcode') and access_token['errcode'] == 0:
+                # 将tocken持久化
+                with open(tokenfile, 'w') as token:
+                    token.write(access_token['access_token'])
+                return access_token['access_token']
+            else:
+                print("get token error!")
+                return ''
+    else:
+        req = request.Request(url)
+        result = request.urlopen(req)
+        access_token = json.loads(result.read())
+        # 判断是返回了合法的值
+        if access_token.has_key('errcode') and access_token['errcode'] == 0:
+            # 将tocken持久化
+            with open(tokenfile, 'w') as token:
+                token.write(access_token['access_token'])
+            return access_token['access_token']
+        else:
+            print("get token error!")
+            return ''
+
+
+def send_dingding(msg, receiveuser):
+    ##钉钉发消息##
+    access_token = get_token()
+    msg_type = 'text'
+    url = "https://oapi.dingtalk.com/message/send?access_token=%s" % access_token
+    to_users = '|'.join(receiveuser)
+    body_dict = {"touser": to_users, "agentid": 286875730, "msgtype": msg_type, msg_type: {"content": msg}}
+    body = json.dumps(body_dict)
+    headers = {'Content-Type': 'application/json; charset=utf-8'}
+    requests = request.Request(url=url, headers=headers, data=body)
+    response = request.urlopen(requests)
+    resp = response.read()
+    return_json = json.loads(resp)
+    # 判断是返回了合法的值
+    if return_json.has_key('errcode') and return_json['errcode'] == 0:
+        print("send message successful!", return_json)
+    else:
+        print("send message failure!", return_json)
+        error_msg = "===*=发送审批消息失败\n===*=消息内容\n\n{msg}\n\n===*=时间：{time}".format(msg=msg,
+                                                                                 time=datetime.datetime.now().strftime(
+                                                                                     '%Y-%m-%d %H:%M:%S'))
